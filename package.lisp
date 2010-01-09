1af92273b227355a8b68aa1b29f07af946c8863c
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2010-01-09 07:18:27 -0500

    changed package.lisp to reflect previous change

--- package.lisp
+++ package.lisp
@@ -1,3 +1,3 @@
 (defpackage #:cl-fs-blog
   (:use #:cl #:html-template)
-  (:export "template->file" "make-all" "*blog-data*" "*templates-for-compilation*"))
+  (:export "template->file" "make-static" "*blog-data*" "*templates-for-compilation*"))
