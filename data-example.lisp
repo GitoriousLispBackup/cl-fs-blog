84d8bab70b07cc885356a00e4b43741aa0bedcd8
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2010-01-09 02:47:01 -0500

    gitignore changes to deal with data and templates-for-compilation; examples for these files
    Signed-off-by: Ian McEwen <ianmcorvidae@ianmcorvidae.net>

---
+++ data-example.lisp
@@ -0,0 +1,6 @@
+(:head-title "Example Blog"
+    :blog-title "Example Blog"
+    :stylesheet "style.css"
+    :body-background-color "#FFFFF0"
+    :blog-posts ((:post-title "Test post one" :post-text "Here is the first test post")
+		 (:post-title "Test post two" :post-text "Here is the second")))
