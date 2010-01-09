b8be0d6eda8cccc0eba71e18579956ac17c24ae8
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2010-01-08 20:36:29 -0500

    added template dir and made it the default template directory
    Signed-off-by: Ian McEwen <ianmcorvidae@ianmcorvidae.net>

--- generate.lisp
+++ generate.lisp
@@ -1,5 +1,7 @@
 (in-package #:ihm-blog)
 
+(setq *default-template-pathname* #p"templates/")
+
 (defun template->file (file template data)
  (with-open-file (stream file :direction :input :if-exists :overwrite :if-does-not-exist :create)
    (fill-and-print-template template data :stream stream)))
