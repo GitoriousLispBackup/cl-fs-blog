89f6053460c6909d47665766c9387a4628e8886e
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2010-01-08 20:03:48 -0500

    added template->file function
    Signed-off-by: Ian McEwen <ianmcorvidae@ianmcorvidae.net>

--- generate.lisp
+++ generate.lisp
@@ -1,1 +1,5 @@
 (in-package #:ihm-blog)
+
+(defun template->file (file template data)
+ (with-open-file (stream file :direction :input :if-exists :overwrite :if-does-not-exist :create)
+   (fill-and-print-template template data :stream stream)))
